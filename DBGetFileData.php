99211235570c469d1aa845b1f533a853672106f4
Tyler-Ziegenfelder <taziegenfelder@electro-matic.com>
2023-06-02 15:31:01 +0000

    new save

--- DBGetFileData.php
+++ DBGetFileData.php
@@ -7,15 +7,17 @@ require_once('./DBConnection.php');
  * @property array $files
  * @property mysqli $mysqli
  */
-class DBGetSegmentData{
+class DBGetFileData{
     public array $files;
     public mysqli $mysqli;
     public $id;
 
-    public function go($id) 
+    public function go() 
     {
-        $this->id = $id;
         $conn = new DBConnection();
+        $sql = "SELECT * FROM current_file";
+        $current_file = $conn->mysqli->query($sql)->fetch_assoc();
+        $this->id = $current_file['id'];
         $sql = "SELECT * FROM files";
         $result = $conn->mysqli->query($sql);
         return $this->reponse_to_interface($result);
@@ -25,12 +27,10 @@ class DBGetSegmentData{
     {
         while($row = $response->fetch_assoc()) {
             if ($this->id == $row['id']){
-                $tempvar = new DBInterfaceSegment($row['id'],$row['hold_time'],$row['scroll_speed'],$row['vertical'],$row['mode']);
+                $tempvar = new DBInterfaceFile($row['id'],$row['hold_time'],$row['scroll_speed'],$row['vertical'],$row['mode']);
                 if (empty($this->files)){
                     $this->files = array($tempvar);
-                }
-                else{
-                    array_push($this->files, $tempvar);
+                    break;
                 }
             }
         }
--- DBGetRowData.php
+++ DBGetRowData.php
@@ -4,11 +4,11 @@ require_once('./DBInterfaceRow.php');
 require_once('./DBConnection.php');
 
 /**
- * @property array $segments
+ * @property array $row_data    
  * @property mysqli $mysqli
  */
-class DBGetSegmentData{
-    public array $rows;
+class DBGetRowData{
+    public array $row_data;
     public mysqli $mysqli;
     public $file_id;
 
@@ -16,7 +16,7 @@ class DBGetSegmentData{
     {
         $this->file_id = $file_id;
         $conn = new DBConnection();
-        $sql = "SELECT * FROM rows_data";
+        $sql = "SELECT * FROM _rows";
         $result = $conn->mysqli->query($sql);
         return $this->reponse_to_interface($result);
     }
@@ -25,12 +25,12 @@ class DBGetSegmentData{
     {
         while($row = $response->fetch_assoc()) {
             if ($this->file_id == $row['file_id']){
-                $tempvar = new DBInterfaceSegment($row['id'],$row['file_id'],$row['number'],$row['font_weight'],$row['font_size'],$row['horizontal_alignment']);
-                if (empty($this->rows)){
-                    $this->rows = array($tempvar);
+                $tempvar = new DBInterfaceRow($row['row_id'],$row['file_id'],$row['number'],$row['font_weight'],$row['font_size'],$row['horizontal_alignment']);
+                if (empty($this->row_data)){
+                    $this->row_data = array($tempvar);
                 }
                 else{
-                    array_push($this->rows, $tempvar);
+                    array_push($this->row_data, $tempvar);
                 }
             }
         }
